08b93aede3ec44b4904088bcefcdbf8438a74180
Alexkibii <alexkibii@gmail.com>
2021-06-28 23:07:21 +0300

    added junit test cases

--- src/test/java/com/csv/app/ComputeCsvApplicationTest.java
+++ src/test/java/com/csv/app/ComputeCsvApplicationTest.java
@@ -1,10 +1,34 @@
 package com.csv.app;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test; 
 
 class ComputeCsvApplicationTest {
+	
+	  private Path workingDir;
+
+	    @Before
+	    public void init() {
+	        this.workingDir = Path.of("", "src/test/java/resources");
+	    }
+
+	    @Test
+	    public void read() throws IOException {
+	        Path file = this.workingDir.resolve("data.csv");
+	        String content = Files.readString(file);
+	        assertThat(content, is("duke"));
+	    }
 
 	@Test
 	void testRead() {
--- src/test/java/com/csv/app/ComputeCsvApplicationTests.java
+++ src/test/java/com/csv/app/ComputeCsvApplicationTests.java
@@ -1,13 +1,45 @@
 package com.csv.app;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ComputeCsvApplicationTests {
 
+	List<String[]> columnValues = new ArrayList<String[]>();
+
+	@Before
+	public void init(){
+	    try {
+	        Scanner scanner = new Scanner(new File("data.csv"));
+	        while(scanner.hasNextLine()) {
+	            String[] values = scanner.nextLine().split(",");
+	            columnValues.add(values);
+	        }
+	    }catch(Exception e) {
+	        //whatever
+	    }
+	}
+	@Test
+	public void myTest(){
+	    //set x to 1 to avoid columns
+	    for(int x = 1; x < columnValues.size(); x++) {
+	        String col1 = columnValues.get(x)[0];
+	        String col2 = columnValues.get(x)[1];
+	        double col3 = Double.parseDouble(columnValues.get(x)[2]);
+	       
+	      System.out.println("Col1 :"+col1+", Col2 :"+col2+", Col3 :"+col3);
+	    }
+	}
 	@Test
 	void contextLoads() {
 	}
+	
 
 }
